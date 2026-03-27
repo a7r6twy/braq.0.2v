9812351ca1987d85a734b73eb21438c43added05
ar7bit <rerer987654321@gmail.com>
2026-03-27 09:58:44 +0300

    Update braq.0.2v.ino
    add a pre-alpha  buttons Control for Calculator

--- braq.0.2v.ino
+++ braq.0.2v.ino
@@ -3,7 +3,7 @@
 LiquidCrystal_I2C lcd(0x27,20,4);
 
 
-byte pins = 9;
+byte pins = 11;
 
 int xc, yc; 
 int xb1 = 5;
@@ -15,101 +15,27 @@ int yb1 = 1, yb2 = 0;
 int speed = 500;
 //inverse relationship betwen val of speed int and speed in the game
 bool lose = false;// true = lose 
-long a;//frist n in the equation
+long  a;//frist n in the equation
 long b;// second n in the equation
 long result;//To display the result
- int n[11]; 
+ byte n1,n2,n3,n4,n5,n6,n7,n8,n9,n10;// the n of button// n10 is n zero 
  byte inputmode;
-int old_n;
-int new_n;
+ //_________________________________________________\\|
+bool cat// i don't know way i name it cat 
+int old_n;//for botton calculator:)
+int new_n;//
+int result2;
+int n[12]// for input read
+//void||\\
 
-void setup() {  
-  
- for (int i = 0; i < pins+1; i++){
-    pinMode(i,INPUT_PULLUP);
-    }  
-  
-  n1=digitalRead(1);
-  n2=digitalRead(2);
-  n3=digitalRead(3);
-  n4= digitalRead(4);
-  n5=digitalRead(5);
-  n6=digitalRead(6);
-  n7=digitalRead(7);
-  n8=digitalRead(8);
-  n9=digitalRead(9);
-  n10=digitalRead(10);
+class apps{
+void app1(); 
+void app2();
 
-  
-  lcd.init();
-  lcd.backlight();
-
-  Serial.begin(9600);
-  Serial.println(F("welcome to braq 0.2"));
-  lcd.print(F("welcome to braq"));
-    
-  delay(2000);
-  lcd.clear();
-  lcd.print(F("power by arduino"));
-  Serial.println(F("power by arduino"));
-  
-  delay(760);
-  lcd.clear();
-  lcd.print(F("by ar7bit"));
-  Serial.println(F("by ar7bit"));
-
-  delay(1000);
-  lcd.clear();
- 
-
-  Serial.println(F("<<For any questions go to my githup and i'll answeryou>>"));
-  delay(1000);
- Serial.println(F("<______________________________________________________>>"));
-  Serial.println(F("My accounts>>>>"));
-  delay(500);
-  Serial.println(F("email||rerer987654321@gamil.com"));
-  delay(250);
-  Serial.println(F("Githup|ar7bit"));
-  delay(760);
-  Serial.println(F("enjoy"));
-delay(1000);
 }
 
-void loop() {
 
-lcd.print("chose your cntrol");
- lcd.print("1.seria\2.buttons");
-lcd.print("chose with button (1,2)");
-  if(n1 == 1||n2 == 0){
-  inputmode = 1;// Serial
-  }
-  else if(n2 == 1|| n1 == 0 ){
-inputmode= 0;//button
-
-  
-  }
-  else if(Serial.available() <= 0){
-    if(Serial.Read() = 's'){
-      inputmode = 1;
-    }
-  }
-
-
-
-  Serial.println(F("<_____________________________________________________>>"));  
-  Serial.println(F("app list"));
-  Serial.println(F("1.calculator"));
-  Serial.println(F("2.car game(comeing soon....)"));
-  lcd.clear();
-  lcd.print("app list");
-  delay(1000);
-  lcd.clear();
-  lcd.setCursor(0,1);
-  lcd.print("1.calculator");
-  lcd.setCursor(0,2);
-  lcd.print("2.car game(comeing soon....)");
-
-  while(Serial.available() <= 0){}
+apps::app1(){ 
    
   int s = Serial.read();
 switch(inputmode){
@@ -214,60 +140,163 @@ case '1':
   break;
    
    case'0' : 
+  
+  
    lcd.claer();
    Serial.println(F((<______________________________________________________>>)); 
    Serial.println("calculator||buttons");
    lcd.print("calculator||buttons");
    delay(1000);
-   lcd.print("enter a (the nmp)");
-   Serial.println("enter a (the nmp)");
-   
-}
-  if(s == '2'){
-   lcd.clear();
-   lcd.print("game of car");
-   Serial.println("game of car"); 
-   delay(1000);
-   
-   while(lose == false){
-    delay(speed);
-    xc++;
-    xb1--;
-    xb2--; 
-    
-    if(Serial.available() > 0){
-      int s = Serial.read();
-      if(s == 'w'){ yc = 0; }
-      if(s == 's'){ yc = 1; }
-    }
-
-  Serial.println("connect lcd to play a game");
-  lcd.clear();
-  lcd.setCursor(xc,yc);
-  lcd.print("C");
-  lcd.setCursor(xb1,yb1);
-  lcd.print("T");
-  lcd.setCursor(xb2,yb2);
-  lcd.print("T");
-
-    if((xc == xb1 && yc == yb1) || (xc == xb2 && yc == yb2)){
-    int skip=2000;
-    lose = true;
-    xc = 0;
-    yc = 0;
-    xb1 = 5;
-    yb1 = 0;
-    xb2 = 18;
-    yb2 = 1;
-    lcd.setCursor(8,0);
-    lcd.print("lose :(");
-    lcd.setCursor(8,1);
-    lcd.print( "prees s to skip"); 
-    if(s == 's' ){
-      skip = 1;
-    }
-   delay(skip);
+   lcd.print("enter a ");
+   Serial.println("enter a ");
+  while()
+  for(int i=0;i<12,i++){
+  if(n[i] == 0){
+    delay(225);
+    switch(i){
+    case 1:
+    new_n = 1;
+    result2=old_n*10+new_n;
+    old_n=new_n;
+    new_n = 0;
+    break;
+    case 2:
+    new_n = 2;
+    result2=old_n*10+new_n;
+    old_n=new_n;
+    new_n=0;    
+    break;
+    case 3:
+    new_n=3;
+    result2=old_n*10+new_n;
+    old_n=new_n;
+    new_n=0;
+    break;
+    case 4;
+    new_n=4;
+    result2=old_n*10+new_n;
+    old_n=new_n;
+    new_n=0;
+    break;
+    case 5:
+    new_n=5;
+    result2=old_n*10+new_n;
+    old_n=new_n;
+    new_n=0;
+    break;
+    case 6:
+    new_n=6;
+    result2=old_n*10+new_n;
+    old_n=new_n;
+    new_n=0;
+    break;
+    case 7:
+    new_n=7;
+    result2=old_n*10+new_n;
+    old_n=new_n;
+    new_n=0;
+    break;
+    case 8:
+    new_n=8;
+    result2=old_n*10+new_n;
+    old_n=new_n;
+    new_n=0; 
+      
     }
    }
+  } 
+ }
+}
+
+
+void setup() {  
+  
+ for (int i = 0; i < pins+1; i++){// make pins input 
+    pinMode(i,INPUT_PULLUP);
+    }  
+  
+  n[1]=digitalRead(1);//
+  n[2]=digitalRead(2);
+  n[3]=digitalRead(3);
+  n[4]= digitalRead(4);
+  n[5]=digitalRead(5);
+  n[6]=digitalRead(6);
+  n[7]=digitalRead(7);
+  n[8]=digitalRead(8);
+  n[9]=digitalRead(9);
+  n[10]=digitalRead(10);
+  n[11]=digitalRead(10);//
+  
+  lcd.init();
+  lcd.backlight();
+
+  Serial.begin(9600);
+  Serial.println(F("welcome to braq 0.2"));
+  lcd.print(F("welcome to braq"));
+    
+  delay(2000);
+  lcd.clear();
+  lcd.print(F("power by arduino"));
+  Serial.println(F("power by arduino"));
+  
+  delay(760);
+  lcd.clear();
+  lcd.print(F("by ar7bit"));
+  Serial.println(F("by ar7bit"));
+
+  delay(1000);
+  lcd.clear();
+ 
+
+  Serial.println(F("<<For any questions go to my githup and i'll answeryou>>"));
+  delay(1000);
+ Serial.println(F("<______________________________________________________>>"));
+  Serial.println(F("My accounts>>>>"));
+  delay(500);
+  Serial.println(F("email||rerer987654321@gamil.com"));
+  delay(250);
+  Serial.println(F("Githup|ar7bit"));
+  delay(760);
+  Serial.println(F("enjoy"));
+delay(1000);
+}
+
+void loop() {
+
+lcd.print("chose your cntrol");
+ lcd.print("1.seria\2.buttons");
+lcd.print("chose with button (1,2)");
+  if(n[1] == 0 & n[2] == 1){
+  inputmode = 1;// serial  
   }
+  else if(n[2] == 0 & n[1] == 1 ){
+inputmode= 0;//button
+
+  
+  }
+  else if(Serial.available() <= 0){
+    if(Serial.Read() = 's'){
+      inputmode = 1;
+    }
+  }
+
+
+
+  Serial.println(F("<_____________________________________________________>>"));  
+  Serial.println(F("app list"));
+  Serial.println(F("1.calculator"));
+  Serial.println(F("2.car game(comeing soon....)"));
+  lcd.clear();
+  lcd.print("app list");
+  delay(500);
+  lcd.clear();
+  lcd.setCursor(0,1);
+  lcd.print("1.calculator");
+  lcd.setCursor(0,2);
+  lcd.print("2.car game");
+  // show app list 
+  if(n[1]==0){
+    app1();
+  }
+  
 }
